37085de01d8dc3d644b765960adf5054425a472f
Mridul Gain <mridulgain@gmail.com>
2023-10-07 08:52:10 +0530

    code description added

--- keyboard_off.sh
+++ keyboard_off.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# A script to deactivate input from laptop's internal keyboard. This is useful when your laptop keyboard is malfunctioning.
 id=$(xinput|grep "AT Translated Set 2 keyboard"|awk '{print $7}')
 echo "Floating xinput for ${id: -2}"
 xinput float ${id: -2}
